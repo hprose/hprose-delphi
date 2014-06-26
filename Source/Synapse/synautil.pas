d8317afb4a4ba1d9494368d669cf6793a482d652
andot <mabingyao@gmail.com>
2014-06-26 15:03:36 +0800

    Update Hprose for Delphi
    Change a {$ENDIF} to {$IFEND} in synautil.pas

--- Source/Synapse/synautil.pas
+++ Source/Synapse/synautil.pas
@@ -608,7 +608,7 @@ begin
   x := rpos(':', Value);
   if (x > 0) and ((Length(Value) - x) > 2) then
     Value := Copy(Value, 1, x + 2);
-  Value := ReplaceString(Value, ':', {$IF DEFINED(DELPHIXE_UP) OR DEFINED(FPC)}FormatSettings.{$ENDIF}TimeSeparator);
+  Value := ReplaceString(Value, ':', {$IF DEFINED(DELPHIXE_UP) OR DEFINED(FPC)}FormatSettings.{$IFEND}TimeSeparator);
   Result := -1;
   try
     Result := StrToTime(Value);
