3d1f5953647d481b09e7759cc2301667e661725b
Ma Bingyao <mabingyao@gmail.com>
2016-12-09 17:25:57 +0800

    Fixed a bug of HttpHeader in Synapse HproseHttpClient

--- Source/Synapse/HproseHttpClient.pas
+++ Source/Synapse/HproseHttpClient.pas
@@ -14,7 +14,7 @@
  *                                                        *
  * hprose synapse http client unit for delphi.            *
  *                                                        *
- * LastModified: Dec 7, 2016                              *
+ * LastModified: Dec 9, 2016                              *
  * Author: Ma Bingyao <andot@hprose.com>                  *
  *                                                        *
 \**********************************************************/
@@ -223,7 +223,9 @@ begin
   Header.PutAll(FHeaders);
   HttpHeader := VarToMap(Context['httpHeader']);
   if (Assigned(HttpHeader)) then
-    Header.PutAll(HttpHeader);
+    Header.PutAll(HttpHeader)
+  else
+    HttpHeader := TCaseInsensitiveHashMap.Create;
   for I := 0 to Header.Count - 1 do
     HttpSend.Headers.Values[Header.Keys[I]] := Header.Values[I];
   HttpSend.KeepAlive := FKeepAlive;
